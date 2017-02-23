2e557b770aff046878ebc3198912a351bb97d167
Adrian Sims <asims@parkeon.com>
2017-02-23 16:50:17 +0000

    Added dictionary to store relevant FF records

--- RCSFlowReaderWPF/RCSViewModel.cs
+++ RCSFlowReaderWPF/RCSViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,21 +10,91 @@ using System.Xml;
 
 namespace RCSFlowReaderWPF
 {
+    class Flow
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string Route { get; set; }
+    }
+
+    class FF
+    {
+        /// <summary>
+        /// f attribute
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// u attribute
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// p attribute
+        /// </summary>
+        public DateTime QuoteDate { get; set; }
+
+        /// <summary>
+        /// k attribute
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// s attribute
+        /// </summary>
+        public string SeasonDetails { get; set; }
+    }
+
     class RCSViewModel : DependencyObject
     {
-        public int FElementCount
+        Dictionary<Flow, Dictionary<string, List<FF>>> flowdic = new Dictionary<Flow, Dictionary<string, List<FF>>>();
+      
+        public bool FlowDone
         {
-            get { return (int)GetValue(FElementCountProperty); }
-            set { SetValue(FElementCountProperty, value); }
+            get { return (bool)GetValue(FlowDoneProperty); }
+            set { SetValue(FlowDoneProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for FElementCount.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty FElementCountProperty =
-            DependencyProperty.Register("FElementCount", typeof(int), typeof(RCSViewModel), new PropertyMetadata(0));
+        // Using a DependencyProperty as the backing store for FlowDone.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FlowDoneProperty =
+            DependencyProperty.Register("FlowDone", typeof(bool), typeof(RCSViewModel), new PropertyMetadata(false));
+
+
+
+        public int FElementTotalCount
+        {
+            get { return (int)GetValue(FElementTotalCountProperty); }
+            set { SetValue(FElementTotalCountProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FElementTotalCount.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FElementTotalCountProperty =
+            DependencyProperty.Register("FElementTotalCount", typeof(int), typeof(RCSViewModel), new PropertyMetadata(0));
+        
+        public int FElementRelevantCount
+        {
+            get { return (int)GetValue(FElementRelevantCountProperty); }
+            set { SetValue(FElementRelevantCountProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FElementRelevantCount.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FElementRelevantCountProperty =
+            DependencyProperty.Register("FElementRelevantCount", typeof(int), typeof(RCSViewModel), new PropertyMetadata(0));
+
+        public int TElementCount
+        {
+            get { return (int)GetValue(TElementCountProperty); }
+            set { SetValue(TElementCountProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TElementCount.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TElementCountProperty =
+            DependencyProperty.Register("TElementCount", typeof(int), typeof(RCSViewModel), new PropertyMetadata(0));
 
         enum RCSFlowStates { Init, RCSFlowData, Header, FlowData, F, T, FF, };
         private async Task<int> ReadRCSFlowInfo()
         {
+            FElementTotalCount = FElementRelevantCount = TElementCount = 0;
             using (var streamreader = new StreamReader("s:/RCS_R_F_170108_00555.xml"))
             {
                 var xr = XmlTextReader.Create(streamreader, new XmlReaderSettings { Async = true });
@@ -31,22 +102,83 @@ namespace RCSFlowReaderWPF
 
                 int fm1Count = 0;
 
+                var flow = new Flow { Origin = "****", Destination = "****", Route = "*****" };
+                string ticketType = "";
+
                 while (await xr.ReadAsync())
                 {
                     if (xr.NodeType == XmlNodeType.Element)
                     {
                         if (xr.Name == "F")
                         {
+                            FElementTotalCount++;
                             fm1Count = 0;
+                            flow = new Flow
+                            {
+                                Origin = xr.GetAttribute("o"),
+                                Destination = xr.GetAttribute("d"),
+                                Route = xr.GetAttribute("r")
+                            };
+                        }
+                        else if (xr.Name == "T")
+                        {
+                            TElementCount++;
+                            ticketType = xr.GetAttribute("t");
                         }
                         else if (xr.Name == "FF")
                         {
                             if (xr.HasAttributes)
                             {
                                 var fmatt = xr.GetAttribute("fm");
-                                if (fmatt != null && fmatt == "00001")
+                                var key = xr.GetAttribute("k");
+                                if (fmatt != null && key != null && fmatt == "00001")
                                 {
                                     fm1Count++;
+
+                                    DateTime endDate, startDate, quoteDate;
+
+                                    var rawdate = "20" + xr.GetAttribute("u");
+                                    if (DateTime.TryParseExact(rawdate, "yyyymmdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out endDate))
+                                    {
+                                        if (endDate.Date >= DateTime.Now.Date)
+                                        {
+                                            rawdate = "20" + xr.GetAttribute("f");
+                                            if (DateTime.TryParseExact(rawdate, "yyyymmdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out startDate))
+                                            {
+                                                var pattribute = xr.GetAttribute("p");
+                                                if (string.IsNullOrWhiteSpace(pattribute))
+                                                {
+                                                    quoteDate = DateTime.Now;
+                                                }
+                                                else
+                                                {
+                                                    rawdate = "20" + pattribute;
+                                                    if (!DateTime.TryParseExact(rawdate, "yyyymmdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out quoteDate))
+                                                    {
+                                                        quoteDate = DateTime.Now;
+                                                    }
+                                                }
+                                                var season = xr.GetAttribute("s");
+                                                var ff = new FF
+                                                {
+                                                    EndDate = endDate,
+                                                    StartDate = startDate,
+                                                    QuoteDate = quoteDate,
+                                                    SeasonDetails = season,
+                                                    Key = key
+                                                };
+                                                if (!flowdic.ContainsKey(flow))
+                                                {
+                                                    flowdic[flow] = new Dictionary<string, List<FF>>();
+                                                }
+                                                if (!flowdic[flow].ContainsKey(ticketType))
+                                                {
+                                                    flowdic[flow][ticketType] = new List<FF>();
+                                                }
+                                                flowdic[flow][ticketType].Add(ff);
+                                            }
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -58,18 +190,23 @@ namespace RCSFlowReaderWPF
                             if (fm1Count > 0)
                             {
                                 fm1Count = 0;
-                                FElementCount++;
+                                FElementRelevantCount++;
                             }
                         }
                     }
                 }
             }
-            return FElementCount;
+            return FElementRelevantCount;
+        }
+        private async void StartRead()
+        {
+            await ReadRCSFlowInfo();
+            FlowDone = true;
         }
 
         public RCSViewModel()
         {
-            ReadRCSFlowInfo();
+            StartRead();
         }
     }
 }
