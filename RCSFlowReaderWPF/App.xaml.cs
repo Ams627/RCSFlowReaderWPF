facd776315704d95a7c849917a8c22e13588b85c
Ams <adriansims62781@gmail.com>
2017-02-23 07:36:17 +0000

    Initial Commit

---
+++ RCSFlowReaderWPF/App.xaml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+
+namespace RCSFlowReaderWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var window = new MainWindow();
+            window.Show();
+        }
+    }
+}
---
+++ RCSFlowReaderWPF/RCSViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+
+namespace RCSFlowReaderWPF
+{
+    class RCSViewModel : DependencyObject
+    {
+        public int FElementCount
+        {
+            get { return (int)GetValue(FElementCountProperty); }
+            set { SetValue(FElementCountProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FElementCount.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FElementCountProperty =
+            DependencyProperty.Register("FElementCount", typeof(int), typeof(RCSViewModel), new PropertyMetadata(0));
+
+        enum RCSFlowStates { Init, RCSFlowData, Header, FlowData, F, T, FF, };
+        private async Task<int> ReadRCSFlowInfo()
+        {
+            using (var streamreader = new StreamReader("s:/RCS_R_F_170108_00555.xml"))
+            {
+                var xr = XmlTextReader.Create(streamreader, new XmlReaderSettings { Async = true });
+                xr.MoveToContent();
+
+                int fm1Count = 0;
+
+                while (await xr.ReadAsync())
+                {
+                    if (xr.NodeType == XmlNodeType.Element)
+                    {
+                        if (xr.Name == "F")
+                        {
+                            fm1Count = 0;
+                        }
+                        else if (xr.Name == "FF")
+                        {
+                            if (xr.HasAttributes)
+                            {
+                                var fmatt = xr.GetAttribute("fm");
+                                if (fmatt != null && fmatt == "00001")
+                                {
+                                    fm1Count++;
+                                }
+                            }
+                        }
+                    }
+                    else if (xr.NodeType == XmlNodeType.EndElement)
+                    {
+                        if (xr.Name == "F")
+                        {
+                            if (fm1Count > 0)
+                            {
+                                fm1Count = 0;
+                                FElementCount++;
+                            }
+                        }
+                    }
+                }
+            }
+            return FElementCount;
+        }
+
+        public RCSViewModel()
+        {
+            ReadRCSFlowInfo();
+        }
+    }
+}
